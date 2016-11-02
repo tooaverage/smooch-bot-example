df069595dbbd6cab01395e4efe02a52c73fd60bf
Jaycee-Ann Day <jayceeannday@gmail.com>
2016-11-02 23:47:05 +0100

    revert

--- console/index.js
+++ console/index.js
@@ -6,7 +6,6 @@ const MemoryLock = smoochBot.MemoryLock;
 const Bot = smoochBot.Bot;
 const Script = smoochBot.Script;
 const StateMachine = smoochBot.StateMachine;
-const fs = require('fs');
 
 class ConsoleBot extends Bot {
     constructor(options) {
@@ -19,23 +18,35 @@ class ConsoleBot extends Bot {
             resolve();
         });
     }
-
-    sendImage(imageFile) {
-        return new Promise((resolve) => {
-            console.log("Image(" + imageFile + ")");
-
-            console.log(imageFile);
-            var real = fs.realpathSync(imageFile);
-            let source = fs.readFileSync(real);
-
-            console.log(source);
-
-            resolve();
-        });
-    }
 }
 
-const script = require('../script');
+const script = new Script({
+    start: {
+        receive: (bot) => {
+            return bot.say('Hi! I\'m Smooch Bot!')
+                .then(() => 'askName');
+        }
+    },
+
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name'),
+        receive: (bot, message) => {
+            const name = message.text.trim();
+            bot.setProp('name', name);
+            return bot.say(`I'll call you ${name}! Great!`)
+                .then(() => 'finish');
+        }
+    },
+
+    finish: {
+        receive: (bot, message) => {
+            return bot.getProp('name')
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
+                .then(() => 'finish');
+        }
+    }
+});
 
 const userId = 'testUserId';
 const store = new MemoryStore();
@@ -54,8 +65,8 @@ const stateMachine = new StateMachine({
 
 process.stdin.on('data', function(data) {
     stateMachine.receiveMessage({
-            text: data.toString().trim()
-        })
+        text: data.toString().trim()
+    })
         .catch((err) => {
             console.error(err);
             console.error(err.stack);
